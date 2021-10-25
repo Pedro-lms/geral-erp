0998984bbd82097114b6aa6b05fac7ad7acbcd5c
Pedro-lms <pedrodaluzmachado@gmail.com>
2021-10-24 22:07:03 -0300

    Changes of security: Autentication

--- src/br/com/framwork/hibernate/session/HibernateUtil.java
+++ src/br/com/framwork/hibernate/session/HibernateUtil.java
@@ -25,7 +25,7 @@ public class HibernateUtil implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
-	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/postgres";
+	public static String JAVA_COMP_ENV_JDBC_DATA_SOURCE = "java:/comp/env/jdbc/datasource";
 	
 	private static SessionFactory sessionFactory = buildSessionFactory();
 	
